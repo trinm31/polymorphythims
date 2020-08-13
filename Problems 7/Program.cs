3a0c50f0cfd19ce89e0efdd1cb82002325e0d073
nmtri31 <trinmgcd19833@fpt.edu.vn>
2020-08-13 23:22:09 +0700

    tri mac

--- Problems 7/Program.cs
+++ Problems 7/Program.cs
@@ -6,7 +6,7 @@ namespace Problems_7
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello tri");
         }
     }
 }
